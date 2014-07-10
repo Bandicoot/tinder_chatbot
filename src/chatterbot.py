af8e49b9df7ef268cac107255ab8d7aa0f22ac86
Andrew Delikat <andrewdelikat@gmail.com>
2014-07-09 22:51:28 -0700

    Add choose_match

--- src/chatterbot.py
+++ src/chatterbot.py
@@ -1,6 +1,14 @@
+import time, random
 from tinder_client import TinderClient
 
 class ChatterBot(object):
 
     def __init__(self, token):
-        self.tinder_client = TinderClient()
+        self.tinder_client = TinderClient(token)
+        self.recs = self.tinder_client.get_recs()['results']
+        self.matches = {}
+
+    def choose_match(self):
+        rec = self.recs.pop(0)['_id']
+        handler = random.choice([self.tinder_client.like, self.tinder_client.dislike])
+        handler(rec)
